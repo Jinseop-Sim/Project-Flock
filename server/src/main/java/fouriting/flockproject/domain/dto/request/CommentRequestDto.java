a55db79ab4e3f7198694433ec187e5b07b7ab7fc
Jinseop-Sim <jjanggu1565@gmail.com>
2022-08-21 05:18:56 +0900

    [Modify] Hotifx Comment create time

--- server/src/main/java/fouriting/flockproject/domain/dto/request/CommentRequestDto.java
+++ server/src/main/java/fouriting/flockproject/domain/dto/request/CommentRequestDto.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Builder
 public class CommentRequestDto {
     private String contents;
+    private String createTime;
 
     public Comment sendComment(Member member, String author, Webtoon webtoon){
         return Comment.builder()
@@ -21,7 +22,7 @@ public class CommentRequestDto {
                 .webtoon(webtoon)
                 .author(author)
                 .contents(contents)
-                .postTime(LocalDateTime.now())
+                .postTime(createTime)
                 .build();
     }
 }
